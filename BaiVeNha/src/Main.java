e11b3c90845a8f2b4309044310c7a24a72868330
manhtoan2512 <121614206+manhtoan2512@users.noreply.github.com>
2023-03-03 04:38:52 +0700

    Update Main.java

--- BaiVeNha/src/Main.java
+++ BaiVeNha/src/Main.java
@@ -1,5 +1,11 @@
+import logic.MenuManagementLogic;
+import logic.QuanLyKhachHangLogic;
+
 public class Main {
     public static void main(String[] args) {
+
+            MenuManagementLogic menuManagementLogic = new MenuManagementLogic();
+            menuManagementLogic.menu();
 
     }
 }
